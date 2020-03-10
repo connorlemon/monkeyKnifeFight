8eb12890a24c319d95bd0bac53125d830a568d45
Connor Lemon <connorlemon@Connors-iMac.local>
2020-03-10 15:17:05 -0700

    complete

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -5,15 +5,49 @@ var player2val = "more";
 
 //Function to print to console, no back end
 //input type = email validation does not work with this function
-function printFormValues(form){
+// function printFormValues(form){
+//     if(!form.elements.player1.value || !form.elements.player2.value){
+//         errorMsg.textContent = "You must make a selection above";
+//         return false;
+//     } else if(form.elements.email2.value !== form.elements.email1.value){
+//         errorMsg.textContent="Your email addresses do not match";
+//         return false;
+//     } else if(formelements.email2.value === form.elements.email1.value){
+//         errorMsg.textContent="Success!";
+//         errorMsg.style.color="green";
+//         return false;
+//     } else {
+//         errorMsg.textContent = " ";
+//     }
+
+//     if(form.elements.player1.value !== "more"){
+//         player1val = "less";
+//     }
+//     if(form.elements.player2.value !== "more"){
+//         player2val = "less";
+//     }
+//     console.log("Lebron = " + player1val.toUpperCase());
+//     console.log("Lillard = " + player2val.toUpperCase());
+//     console.log(form.elements.email1.value);
+// }
+
+// JS for form submission to Back End
+form.addEventListener('submit', function(event){
     if(!form.elements.player1.value || !form.elements.player2.value){
         errorMsg.textContent = "You must make a selection above";
-        return false;
+        errorMsg.style.color="#900";
+        event.preventDefault();
     } else if(form.elements.email2.value !== form.elements.email1.value){
         errorMsg.textContent="Your email addresses do not match";
-        return false;
+        errorMsg.style.color="#900";
+        event.preventDefault();
+    } else if(form.elements.email2.value === form.elements.email1.value){
+        errorMsg.textContent="Success!";
+        errorMsg.style.color="green";
+        event.preventDefault();
     } else {
-        errorMsg.textContent = "";
+        errorMsg.textContent = "Hidden";
+        errorMsg.style.color="#E5E4E4";
     }
 
     if(form.elements.player1.value !== "more"){
@@ -25,26 +59,4 @@ function printFormValues(form){
     console.log("Lebron = " + player1val.toUpperCase());
     console.log("Lillard = " + player2val.toUpperCase());
     console.log(form.elements.email1.value);
-}
-
-//JS for form submission to Back End
-form.addEventListener('submit', function(event){
-    if(!event.elements.player1.value || !event.elements.player2.value){
-        errorMsg.textContent = "You must make a selection above";
-        event.preventDefault();
-    } else if(event.elements.email2.value !== event.elements.email1.value){
-        errorMsg.textContent="Your email addresses do not match";
-        event.preventDefault();
-    } else {
-        errorMsg.textContent = "";
-    }
-
-    if(event.elements.player1.value !== "more"){
-        player1val = "less";
-    }
-    if(event.elements.player2.value !== "more"){
-        player2val = "less";
-    }
-
-    //Create Database Entry
 })
